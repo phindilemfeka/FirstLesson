f80def682be04358470d4bba074a8730bb034ca4
phindilemfeka <phindileportia20@gmail.com>
2020-06-20 23:03:02 +0200

    getting there

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -2,34 +2,16 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
 
-    static void multiplyInput(){
+    static void multiplyInput(int input,int limit){
 
-        int input , limit;
 
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Please enter an integer of your choice:");
-
-        input = reader.nextInt();
-
-        System.out.println("Enter limit of your multiplication  table");
-
-        limit =reader.nextInt();
-
-//        for (int i=1; i<limit; i++){ // prints the first 15 multiplies of 3
-//            System.out.println(i*input);
-//
-//        }
-
-        for (int i=1; i<limit; i++){ //prints multiplies of 3 ranging from 1-15.
-            if (i*input<=limit){
-                System.out.println(i*input);
-            }
+        // responsible for printing multiplication table of given number input up to a certain limit
+        for (int i=1; i<=limit; i++){
+            System.out.println(input+" "+"*"+" "+i+" "+"="+i*input);
         }
-
     }
 
     public static void main(String[] args) {
-        multiplyInput();
+        multiplyInput(5,10);
     }
 }
--- src/com/company/Diamond.java
+++ src/com/company/Diamond.java
@@ -1,8 +1,28 @@
-package com.company;
-
-public class Diamond {
-    static void drawDiamond(){
-
-
-    }
-}
+//package com.company;
+//
+//public class Diamond {
+//    static void drawDiamond(int n){
+//
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = 0; j < i + 1; ++j) {
+//                System.out.print(c);
+//            }
+//            System.out.println();
+//        }
+//
+//        // print the lower triangle
+//        for (int i = n - 1; i > 0; --i) {
+//            for (int j = 0; j < i; ++j) {
+//                System.out.print(c);
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//        Diamond.drawDiamond(4);
+//    }
+//}
